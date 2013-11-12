4c46d0aca5c702ad3d1c01b4d43dddac81aadd0d
Felix Tioh <felixsagitta@gmail.com>
2013-11-12 09:08:59 +0800

    change to correct site url

--- server/app/mailers/user_mailer.rb
+++ server/app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ActionMailer::Base
   default from: "contactus@creativesatwork.me"
 
-  SiteUrl = 'http://staging.creativesatwork.me'
+  SiteUrl = 'http://creativesatwork.me'
   #SiteUrl = 'http://localhost:3333'
 
   def activate_freelancer_account(user, token)
