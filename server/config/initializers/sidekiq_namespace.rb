fe26c700057be82798b6e3ed33f3dc36bbfa1f9c
Felix Tioh <felixsagitta@gmail.com>
2015-01-03 14:22:27 +0800

    sidekiq check working

--- server/config/initializers/sidekiq_namespace.rb
+++ server/config/initializers/sidekiq_namespace.rb
@@ -1,3 +1,7 @@
+Sidekiq.configure_server do |config|
+  config.redis = { url: 'redis://localhost:6379/0', namespace: 'staging-creativesatwork' }
+end
+
 Sidekiq.configure_client do |config|
-  config.redis = {namespace: "staging-creativesatwork"}
+  config.redis = { url: 'redis://localhost:6379/0', namespace: 'staging-creativesatwork' }
 end
